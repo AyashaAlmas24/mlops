c22daad436cebef0c55f57d57636ac45379bddc5
AyashaAlmas24 <ayashaalmas@gmail.com>
2025-07-23 14:59:47 +0530

    app.py

--- app.py
+++ app.py
@@ -1,41 +1,32 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_absolute_error, r2_score
+from flask import Flask, request,render_template
 import joblib
-import mlflow
-import mlflow.sklearn
-mlflow.sklearn.autolog()
-df = pd.read_csv("airbnb_listings.csv")
-x = df.drop(columns=["PricePerNight", "ListingID"])
-y = df["PricePerNight"]
-categorical = ["City", "RoomType"]
-numeric = ["Bedroom", "Bathroom", "GuestsCapacity", "HasWifi", "HasAC", "DistanceFromCityCenter"]
-preprocessor = ColumnTransformer(
-    transformers=[("cat", OneHotEncoder(handle_unknown="ignore"), categorical)],
-    remainder="passthrough"
-)
-model = Pipeline(steps=[
-    ("preprocessor", preprocessor),
-    ("regressor", RandomForestRegressor(n_estimators=100, random_state=42))
-])
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-with mlflow.start_run():
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    mlflow.log_metric("mae", mae)
-    mlflow.log_metric("r2_score", r2)
-    joblib.dump(model, "model.pkl")
-    mlflow.log_artifact("model.pkl")
-    print(f"Model Saved with MAE = {mae}, R² = {r2}")
-#afer executing in the termial type mlflow ui
+import pandas as pd
 
+app=Flask(__name__)
+model=joblib.load('model.pkl')
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method=='POST':
+        data={
+            "City": request.form.get("City"),
+            "Room Type": request.form.get("Room Type"),
+            "Bedrooms": float(request.form.get("Bedrooms")),
+            "Bathrooms": float(request.form.get("Bathrooms")),
+            "GuestsCapacity": int(request.form.get("GuestsCapacity")),
+            "HasWifi": int(request.form.get("HasWifi")),
+            "HasAC": int(request.form.get("HasAC")),
+            "DistanceFromCityCenter": float(request.form.get("DistanceFromCityCenter")),
+            "PricePerNight": float(request.form.get("PricePerNight"))
+        }
+        df=pd.DataFrame([data])
+        prediction=model.predict(df)[0]
+        return render_template('index.html', prediction_text=f'Predicted Price Per Night: ${round(prediction, 2)}')
+    return render_template('index.html')
 
-#accuracy=(tp+tn)/(tp+tn+fp+fn)
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=5000)
